69d39d29100227c7f3eef2e0f1ef6ae7a697f31a
Tia King <tiaking@Timothys-MacBook-Pro.local>
2017-11-18 23:50:17 -0800

    Added Comments

--- flask_unit_test.py
+++ flask_unit_test.py
@@ -25,16 +25,20 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_get_a_user(self):
 
+    	#getting a user
     	response = self.app.get('/users',
     		query_string=dict(name='TJ King'))
 
+    	#decode the response
+
     	response_json = json.loads(response.data.decode())
 
+    	#Here we check the status code
     	self.assertEqual(response.status_code, 200)
 
     def test_post_a_user(self):
 
-    ## Post 2 users to database
+    # posting user to database
     	self.app.post('/users',
                       headers=None,
                       data=json.dumps(dict(
@@ -43,7 +47,7 @@ class FlaskrTestCase(unittest.TestCase):
                       )),
                       content_type='application/json')
     
-    ## 3 Make a get request to fetch the posted user
+    # using the get method to test the success of the post method
 
     	response = self.app.get('/users',
                             query_string=dict(name="Eliel Gordon")
@@ -52,11 +56,12 @@ class FlaskrTestCase(unittest.TestCase):
     # Decode reponse
     	response_json = json.loads(response.data.decode())
     
-    ## Actual test to see if GET request was succesful
-    ## Here we check the status code
+    # if get is successful, we'll get the correct status code
+    # Here we check the status code
     	self.assertEqual(response.status_code, 200)
 
     def test_updating_user(self):
+    	# posting user to database
 
     	self.app.post('/users',
     			headers=None,
@@ -66,20 +71,25 @@ class FlaskrTestCase(unittest.TestCase):
     				)),
     			content_type='application/json')
 
+    	#changing just the name of the user using put method
     	self.app.put('/users',
     				headers=None,
     				data=json.dumps(dict(
     					name="Tia King")),
     			content_type='application/json')
-
+    	#use the get method to query the database using the changed name
     	response = self.app.get('/users',
     						query_string=dict(name="Tia King")
     			)
+
+    	# if get is successful, we'll get the correct status code
+    	# Here we check the status code
 
     	response_json = json.loads(response.data.decode())
     	self.assertEqual(response.status_code, 200)
 
     def test_replace_user(self):
+    	# posting user to database
     	self.app.post('/users',
     			headers=None,
     			data=json.dumps(dict(
@@ -87,50 +97,59 @@ class FlaskrTestCase(unittest.TestCase):
     				age=10
     				)),
     			content_type='application/json')
-
+    	# using the patch method to change all aspects of a user, replacing the inital one
     	self.app.patch('/users',
     			headers=None,
     			data=json.dumps(dict(
     				name="Tia King",
     				age=9)),
     			content_type='application/json')
-
+    	#use the get method to query the database using the changed user details
     	response = self.app.get('/users',
     						query_string=dict(name="Tia King"))
 
+    	# if get is successful, we'll get the correct status code
+    	# Here we check the status code
     	response_json = json.loads(response.data.decode())
     	self.assertEqual(response.status_code, 200)
 
     def test_delete_user(self):
+    	#post a user to database
     	self.app.post('/users',
     			headers=None,
     			data=json.dumps(dict(
     				name="TJ King",
     				)),
     			content_type='application/json')
-
+    	#search for that user and delete them using delete method
     	response = self.app.delete('/users',
     			headers=None,
     			data=json.dumps(dict(
     				name="TJ King",
     				)),
     			content_type='application/json')
-    	# response = self.app.get('users', 
-    	# 					query_string=dict(name="TJ King"))
     	
+    	# if delete is successful, we'll get the correct status code
+    	# Here we check the status code
     	response_json = json.loads(response.data.decode())
     	self.assertEqual(response.status_code, 200)
 
     def test_get_a_trip(self):
+    	 #getting a trip
+
     	response = self.app.get('/trips',
     		query_string=dict(destination='Seoul'))
 
+    	# Decode reponse
     	response_json = json.loads(response.data.decode())
 
+    	# if get is successful, we'll get the correct status code
+    	# Here we check the status code
     	self.assertEqual(response.status_code, 200)
 
     def test_post_a_trip(self):
 
+    	#posting a test trip to database
     	self.app.post('/trips',
                       headers=None,
                       data=json.dumps(dict(
@@ -139,22 +158,23 @@ class FlaskrTestCase(unittest.TestCase):
                       )),
                       content_type='application/json')
     
-    ## 3 Make a get request to fetch the posted user
+    	#Make a get request to fetch the posted trip
 
     	response = self.app.get('/trips',
                             query_string=dict(destination="Seoul")
                         )
                                 
-    # Decode reponse
+    # Decode response
     	response_json = json.loads(response.data.decode())
     
-    ## Actual test to see if GET request was succesful
+    ## if get is successful, we'll get the correct status code
     ## Here we check the status code
     	self.assertEqual(response.status_code, 200)
 
 
     def test_updating_trip(self):
 
+    	#post a test trip to database
     	self.app.post('/trips',
     			headers=None,
     			data=json.dumps(dict(
@@ -163,6 +183,7 @@ class FlaskrTestCase(unittest.TestCase):
     				)),
     			content_type='application/json')
 
+    	#use the put method to change the name of the trip
     	self.app.put('/trips',
     				headers=None,
     				data=json.dumps(dict(
@@ -170,15 +191,20 @@ class FlaskrTestCase(unittest.TestCase):
                           trip_day_amount=5)),
     				content_type='application/json')
 
+    	#use get method to fetch updated trip
     	response = self.app.get('/trips',
     						query_string=dict(destination="San Francisco")
     			)
 
+    	#if get is successful, we'll get the correct status code
+    	#Here we check the status code
     	response_json = json.loads(response.data.decode())
     	self.assertEqual(response.status_code, 200)
 
 
     def test_replace_a_trip(self):
+
+    	#post test trip to database
 		self.app.post('/trips',
     			headers=None,
     			data=json.dumps(dict(
@@ -186,30 +212,39 @@ class FlaskrTestCase(unittest.TestCase):
                     trip_day_amount=5
     				)),
     			content_type='application/json')
+		#use patch method to completely change the trip details
 		self.app.patch('/trips',
     			headers=None,
     			data=json.dumps(dict(
     				destination="San Francisco",
     				trip_day_amount=9)),
     			content_type='application/json')
+		#use get method to fetch the new trip
 		response = self.app.get('/trips',
     						query_string=dict(name="San Francisco"))
+
+		#if get is successful, we'll get the correct status code
+		#here we check the status code
 		response_json = json.loads(response.data.decode())
 		self.assertEqual(response.status_code, 200)
 
     def test_delete_a_trip(self):
+
+    	#post a test trip to the database
     	self.app.post('/trips',
     			headers=None,
     			data=json.dumps(dict(
     				destination="Seoul",
     				)),
     			content_type='application/json')
+    	#use the delete method to remove the posted trip
     	response = self.app.delete('/trips',
     			headers=None,
     			data=json.dumps(dict(
     				destination="Seoul",
     				)),
     			content_type='application/json')
+    	#check status code to see if it worked
     	response_json = json.loads(response.data.decode())
     	self.assertEqual(response.status_code, 200)
 
